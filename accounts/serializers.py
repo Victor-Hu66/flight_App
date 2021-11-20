fb6f72e219e4f8531a8b31a51beeff26a2aa2dcf
ahmet <scienceworker34@gmail.com>
2021-11-20 19:50:39 +0300

    add registration

---
+++ accounts/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers, validators
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    email = serializers.EmailField(
+        required=True,
+        validators=[validators.UniqueValidator(queryset=User.objects.all())]
+        )
+    
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type":"password"}
+        )
+    
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={"input_type":"password"}
+        )
+    
+    
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'password2'          
+        ]
+        
+        extra_kwargs = {
+            "password" : {"write_only":True},
+            "password2" : {"write_only":True}
+        }
+        
+    def create(self, validated_data):
+        password = validated_data.get("password")
+        validated_data.pop("password2")
+        
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError(
+            {"password": "Password fields didn't match."})
+        return data
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+class RegisterApi(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    
+    
